cb986c41e6cf9d174b91775d1196497d381d514a
alind <kashishrawat308@gmail.com>
2023-04-10 22:19:38 +0530

    all changes

--- App.js
+++ App.js
@@ -1,3 +1,29 @@
-const heading = React.createElement("h1", {}, "hiiii");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);h
+// imported react and reactdom from nodemodule folder
+import React from "react";
+import ReactDOM from 'react-dom/client';
+const heading = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading"
+  );
+  const heading1 = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading1"
+  );
+  
+  const container = React.createElement(
+    "div",
+    {
+      id: "container",
+    },
+    [heading, heading1]
+  );
+  // create root using createRoot
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  // passing react element inside root
+  root.render(container);
