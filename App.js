d6aa9574659e5cfa2e83ed05a5980d1109a13346
alind <kashishrawat308@gmail.com>
2023-04-23 21:57:36 +0530

    all commit

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
 // imported react and reactdom from nodemodule folder
 import React from "react";
 import ReactDOM from 'react-dom/client';
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading"
-  );
-  const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading1"
-  );
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading1]
-  );
+
+const Header =()=>{
+  return <>
+  i am header
+  </>
+}
+const App = () => {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
   // create root using createRoot
   const root = ReactDOM.createRoot(document.getElementById("root"));
   // passing react element inside root
-  root.render(container);
+  root.render(<App/>);
